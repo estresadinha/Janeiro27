cec3bee5cb9d99f58c58c31c033eaa407aa7fee2
estresadinha <120022356+estresadinha@users.noreply.github.com>
2023-01-27 11:49:55 -0300

    Add files via upload

---
+++ 27deJaneiro/Program.cs
@@ -0,0 +1,27 @@
+﻿using 27deJaneiro;
+
+ContaCorrente conta1 = new ContaCorrente();
+ContaCorrente conta2 = new ContaCorrente();
+ContaCorrente conta3 = new ContaCorrente();
+
+conta1.nome = "Manuele";
+conta1.agencia = 245;
+conta1.saldo = 7500.25;
+conta1.conta = "147-X";
+conta1.depositar(400);
+
+conta2.nome = "Marcia";
+conta2.agencia = 456;
+conta2.saldo = 500.29;
+conta2.conta = "149-X";
+conta2.depositar(10);
+
+conta3.nome = "Maria";
+conta3.agencia = 789;
+conta3.saldo = 750.30;
+conta3.conta = "150-X";
+conta3.depositar(50);
+
+
+
+
